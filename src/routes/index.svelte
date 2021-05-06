4761bf3c2c08d31787eee2b79451ffb54ec567f4
fabezio <fabezio@outlook..fr>
2021-05-06 08:46:51 +0200

    initialisation

---
+++ src/routes/index.svelte
@@ -0,0 +1,46 @@
+
+
+<script>
+	import Counter from '../components/Counter/index.svelte';
+</script>
+
+<svelte:head>
+	<title>Accueil</title>
+</svelte:head>
+
+<section>
+	<h1>
+		Accueil
+	</h1>
+
+	<p>Pour voir le planning du mois, clique <a href="planning">ici</a><br>ou sur le lien ci-dessus</p>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
