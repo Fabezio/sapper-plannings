af7674367d24e71b7ab79579f07a36fd200241e5
fabezio <fabezio@outlook.fr>
2021-05-16 02:41:32 +0200

    correctif datation

--- src/services/workers.js
+++ src/services/workers.js
@@ -3,20 +3,25 @@ import plannings from '../data/plannings'
 const { mai } = plannings
 const { ADS } = mai
 const formatter = Intl.DateTimeFormat('fr', {
-  weekday: 'short'
-  /* day: "numeric",
-        month: "short",
-        year: "numeric", */
+  weekday: 'short',
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric'
 })
 ADS.map(el => {
-  const thisDay = new Date().setFullYear(2021, 5, el.jour)
-  let weekDay = formatter.format(thisDay)
+  const thisDay = new Date().setFullYear(2021, 4, el.jour)
+  let date = formatter.format(thisDay)
+  const ele = date.split(' ')
+  const weekDay = ele[0]
+  const mois = ele[2]
   // weekDay.length = 3;
   // weekDay.length = 3;
+  console.log(ele)
+  // console.log(el.weekday)
   el.weekday = weekDay
-  // el.weekday = weekDay.slice(0, 3)
-  console.log(el.weekday)
   return el
+  // el.weekday = weekDay.slice(0, 3)
+  // return el
 })
 
 const namesList = []
