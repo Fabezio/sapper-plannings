49edfeffd47138512ee19721edc03431fb813981
fabezio <fabezio@outlook.fr>
2021-05-16 23:06:35 +0200

    cleanup

--- src/routes/planning.svelte
+++ src/routes/planning.svelte
@@ -9,7 +9,6 @@
         return (handleFilter = !handleFilter);
     }
     function selectEmployee(e) {
-        // displayPlanning = true;
         console.log(e.target.innerText);
         worker = worker.toUpperCase();
         console.log(worker);
@@ -36,7 +35,7 @@
 
     const today = new Date().getDate();
     let worker = "";
-    // $: worker = worker.toUpperCase();
+
     $: if (namesList.includes(worker)) displayFilteredList();
 </script>
 
@@ -51,24 +50,6 @@
     {/each}
 </div>
 
-<!-- <div class="flex-filter">
-    {#if handleFilter}
-        <input
-            class="input"
-            type="text"
-            bind:value={worker}
-            placeholder="entre le nom de l'agent"
-        />
-    {:else}
-        <div class="msg">
-            tu peux filter les résultats<br /> en cliquant ci-contre
-        </div>
-    {/if}
-    <button class="round" title="filtrer avec mon nom" on:click={toggleFilter}>
-        <img src={filter} alt="filtre" width="24" height="24" />
-    </button>
-</div> -->
-
 {#if namesList.includes(worker)}
     <Workers arr={adsFilter} />
 {/if}
@@ -77,40 +58,6 @@
 {/if}
 
 <style>
-    .round {
-        border-radius: 50%;
-        padding: 0;
-        width: 48px;
-        height: 48px;
-        border: none;
-        opacity: 0.75;
-    }
-    .round:hover {
-        opacity: 1;
-    }
-    .msg,
-    input {
-        margin-right: 1rem;
-        /* transform: rotateY(180deg); */
-        transition-duration: 250ms;
-        /* transform: fade */
-    }
-    .msg {
-        /* transition-duration: 250ms; */
-        align-self: center;
-    }
-    input {
-        transition-duration: 250ms;
-        border: 1px solid rgba(0, 0, 0, 0.25);
-        padding: 8px 8px;
-        border-radius: 4px;
-        margin-right: 1rem;
-    }
-    .flex-filter {
-        display: flex;
-        justify-content: flex-end;
-    }
-
     @media screen and (max-width: 480px) {
         h2 {
             /* text-align: center; */
